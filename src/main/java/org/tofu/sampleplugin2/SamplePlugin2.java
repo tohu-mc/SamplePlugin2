e3f7d4f3b432d3ca2371c8ba6920db8fc6e57773
Ataru-Kurihara <ataru4101826514@gmail.com>
2024-03-21 15:53:37 +0900

    first commit

---
+++ src/main/java/org/tofu/sampleplugin2/SamplePlugin2.java
@@ -0,0 +1,17 @@
+package org.tofu.sampleplugin2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SamplePlugin2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
